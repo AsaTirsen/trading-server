c760cc7ad0aeb1905f45a2763965f04f55e292c3
Asa Tirsen <asa.tirsen@gmail.com>
2020-11-08 09:31:55 +0100

    changed ports

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const io = require('socket.io')(server, {
   transports: ['websocket', 'polling']
 });
 
-const port = 1343;
+//const port = 1343;
 io.origins(['https://www.traders-galore.asatirsen.me:443'])
-console.log(port);
+//console.log(port);
 //
 // let pinkLady = {
 //   name: "Pink Lady",
