c633ce1479dcbc532cb504a00ae9c6bd3b578bf3
Asa Tirsen <asa.tirsen@gmail.com>
2020-11-11 14:54:02 +0100

    changed setInterval, created connections array

--- app.js
+++ app.js
@@ -22,27 +22,35 @@ let grannySmith = {
     startingPoint: 10,
 };
 
+let connections = [];
+let currentPrice = null;
 
+setInterval(function () {
+    grannySmith.startingPoint = stock.getStockPrice(grannySmith);
+    currentPrice = {
+        time: new Date().toLocaleTimeString(),
+        value: grannySmith.startingPoint
+    }
+    connections.forEach((c) => {
+        c.send(JSON.stringify(currentPrice));
+    })
+}, 10000);
 
 wsServer.on('request', function (request) {
-    const connection = request.accept(null, request.origin);
-    connection.on('message', function (message) {
-    })
-
-    setInterval(function () {
-        grannySmith["startingPoint"] = stock.getStockPrice(grannySmith);
-        let stocks = {
-            time: new Date().toLocaleTimeString(),
-            value: grannySmith["startingPoint"]
-        }
-        console.log(grannySmith["startingPoint"])
-        connection.send(JSON.stringify(stocks));
-        console.log(grannySmith)
-    }, 50000);
-
     console.log("user connected");
+
+    const connection = request.accept(null, request.origin);
+
+    // Immediately send the current price
+    if (currentPrice) {
+        connection.send(JSON.stringify(currentPrice));
+    }
+
+    connections.push(connection);
+
     // user disconnected
     connection.on('close', function () {
+        connections = connections.filter((c) => c !== connection);
         console.log("user disconnected");
     });
 });
